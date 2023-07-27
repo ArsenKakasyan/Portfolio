899a0a6880b1e4cbacd4e1e2f29d419d89be04f4
ArsenKakasyan <archee.busy@gmail.com>
2023-07-27 15:41:02 +0300

    path bug fix

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,7 @@ import Navbar from "./navBar/Navbar"
 import Intro from "./intro/Intro";
 import About from "./intro/about/About"
 import Service from "./service/Service";
-import ServiceList from "./ServiceList/ServiceList";
+import ServiceList from "./serviceList/ServiceList"
 import Contact from "./contact/Contact";
 
 export {
