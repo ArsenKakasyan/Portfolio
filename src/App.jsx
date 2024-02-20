0aa90405c3924e4c8b6e41f8e838f93839e04745
ArsenKakasyan <archee.busy@gmail.com>
2024-02-20 15:57:46 +0300

    analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Analytics } from "@vercel/analytics/react";
 import { LanguageProvider } from "./LanguageContext";
 import { Navbar, Intro, About, ServiceList, Contact } from "./components";
 
@@ -10,6 +11,7 @@ const App = () => {
         <Intro />
         <About />
         <ServiceList />
+        <Analytics />
       </div>
     </LanguageProvider>
   );
